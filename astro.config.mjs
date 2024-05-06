8ed06a6a7d320b885cb8e25d5a7a4ff4d03d920a
Liam Darkmoon <liamdarkmoon@gmail.com>
2024-05-06 16:13:09 -0300

    configured adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
-import react from "@astrojs/react";
+import react from "@astrojs/react"
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'hybrid',
-  adapter: vercel({
+  output: 'server',
+  adapter: vercelServerless({
     maxDuration: 10,
     edgeMiddleware: true,
     imageService: true
